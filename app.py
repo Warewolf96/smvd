ee37ec79f3dc3e3493248501b6c5cc7656e917b4
JARVIS <parth.verma96@gmail.com>
2018-07-14 16:43:43 +0530

    Added all pages

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
 from sqlalchemy.orm import sessionmaker
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 from tabledef import *
 engine = create_engine('sqlite:///tutorial.db', echo=True)
  
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
+
+class ReusableForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
+    email = TextField('Email:', validators=[validators.required(), validators.Length(min=6, max=35)])
+    password = TextField('Password:', validators=[validators.required(), validators.Length(min=3, max=35)])
 
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
@@ -33,9 +39,80 @@ def logout():
     session['logged_in'] = False
     return home()
 
-@app.route('/table2')
-def foobar():
-    return render_template('table2.html')
+@app.route('/yrc')
+def yrc():
+    form = ReusableForm(request.form)
+    return render_template('yrc.html', form=form)
+
+@app.route('/fif')
+def fif():
+    form = ReusableForm(request.form)
+    return render_template('fif.html', form=form)
+
+@app.route('/sa')
+def sa():
+    form = ReusableForm(request.form)
+    return render_template('sa.html', form=form)
+
+@app.route('/ws')
+def ws():
+    form = ReusableForm(request.form)
+    return render_template('ws.html', form=form)
+
+@app.route('/os')
+def os():
+    form = ReusableForm(request.form)
+    return render_template('os.html', form=form)
+
+@app.route('/ic')
+def ic():
+    form = ReusableForm(request.form)
+    return render_template('ic.html', form=form)
+
+@app.route('/icer')
+def icer():
+    form = ReusableForm(request.form)
+    return render_template('icer.html', form=form)
+
+@app.route('/sr')
+def sr():
+    form = ReusableForm(request.form)
+    return render_template('sr.html', form=form)
+
+@app.route('/sg')
+def sg():
+    form = ReusableForm(request.form)
+    return render_template('sg.html', form=form)
+
+@app.route('/ban')
+def ban():
+    form = ReusableForm(request.form)
+    return render_template('ban.html', form=form)
+
+@app.route('/aus')
+def aus():
+    form = ReusableForm(request.form)
+    return render_template('aus.html', form=form)
+
+@app.route('/ar')
+def ar():
+    form = ReusableForm(request.form)
+    return render_template('ar.html', form=form)
+
+@app.route('/bha')
+def bha():
+    form = ReusableForm(request.form)
+    return render_template('bha.html', form=form)
+
+@app.route('/gb')
+def gb():
+    form = ReusableForm(request.form)
+    return render_template('gb.html', form=form)
+
+@app.route('/jtb')
+def jtb():
+    form = ReusableForm(request.form)
+    return render_template('jtb.html', form=form)
 
 @app.route('/shutdown')
 def shutdown():
